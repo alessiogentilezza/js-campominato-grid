d97134e5fd355c761becdca4e634cd85dc5f82d1
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-03 18:04:46 +0100

    inserito colore back-ground

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@ for (let i = 1; i <= 100; i++) {
     let elementoGriglia = document.createElement('div');
     elementoGriglia.classList.add('square');
     elementoGriglia.append(i);
-
     gridDom.append(elementoGriglia);
+
+    elementoGriglia.addEventListener('click',
+        function () {
+            this.classList.toggle('back-ground');
+            console.log();
+        });
+
 }
 
 playGrid.addEventListener('click',
@@ -15,6 +21,7 @@ playGrid.addEventListener('click',
         let displayShow = document.getElementsByClassName('square');
         for (let i = 0; i < displayShow.length; i++) {
             displayShow[i].classList.toggle('d-flex');
+            displayShow[i].classList.remove('back-ground');
         }
 
     });
