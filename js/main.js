bf4a83a2084df6c43aeda89731d1fde196124f77
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-03 16:20:22 +0100

    aggiunto addEventListener

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 const gridDom = document.getElementById('grid');
+const playGrid = document.getElementById('button');
 
-for (let i = 0; i < 100; i++) {
-    const currentElement = document.createElement('div');
-    currentElement.classList.add('square');
+for (let i = 1; i <= 100; i++) {
+    let elementoGriglia = document.createElement('div');
+    elementoGriglia.classList.add('square');
+    elementoGriglia.append(i);
 
-    gridDom.append(currentElement);
-
+    gridDom.append(elementoGriglia);
 }
+
+const displayShow = document.getElementsByClassName('square');
+
+playGrid.addEventListener('click',
+    function () {
+        displayShow.classList.toggle('d-none');
+    });
