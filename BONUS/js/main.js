734fe991ed46910d37f4c68b1c0d86b5ac2ad794
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-03 19:17:13 +0100

    inserita casella selezione

---
+++ BONUS/js/main.js
@@ -0,0 +1,29 @@
+const gridDom = document.getElementById('grid');
+const playGrid = document.getElementById('button');
+
+for (let i = 1; i <= 100; i++) {
+    let elementoGriglia = document.createElement('div');
+    elementoGriglia.classList.add('square');
+    elementoGriglia.append(i);
+
+    gridDom.append(elementoGriglia);
+
+    elementoGriglia.addEventListener('click',
+        function () {
+            this.classList.toggle('back-ground');
+            console.log(this.innerHTML);
+        });
+
+}
+
+
+playGrid.addEventListener('click',
+
+    function () {
+        let displayShow = document.getElementsByClassName('square');
+        for (let i = 0; i < displayShow.length; i++) {
+            displayShow[i].classList.toggle('d-flex');
+            displayShow[i].classList.remove('back-ground');
+        }
+
+    });
