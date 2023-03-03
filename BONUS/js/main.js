4526708870dc1894dcd9ac60afb78b6292637f49
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-03 23:22:58 +0100

    codice pulito, mi manca il quadrato sugli 81 e sui 49 per il resto ok

--- BONUS/js/main.js
+++ BONUS/js/main.js
@@ -1,26 +1,45 @@
 const gridDom = document.getElementById('grid');
 const playGrid = document.getElementById('button');
+const casellaSelezione = document.getElementById('casellaSelezione');
 
-for (let i = 1; i <= 100; i++) {
-    let elementoGriglia = document.createElement('div');
-    elementoGriglia.classList.add('square');
-    elementoGriglia.append(i);
+function griglia(numbreSquare){
 
-    gridDom.append(elementoGriglia);
+    gridDom.innerHTML = '';
 
-    elementoGriglia.addEventListener('click',
+    for (let i = 1; i <= numbreSquare; i++) {        
+        let elementoGriglia = document.createElement('div');
+        elementoGriglia.classList.add('square');
+        elementoGriglia.append(i);
+        gridDom.append(elementoGriglia);
+        
+        elementoGriglia.addEventListener('click',
         function () {
             this.classList.toggle('back-ground');
             console.log(this.innerHTML);
-        });
-
+        });}
 }
-
 
 playGrid.addEventListener('click',
 
     function () {
         let displayShow = document.getElementsByClassName('square');
+        let casellaSelezioneScelta = casellaSelezione.value;
+
+        switch (casellaSelezioneScelta) {
+        
+        case 'Difficoltà-1':
+            griglia(100);
+                break;
+        case 'Difficoltà-2':
+            griglia(81);
+                break;
+        case 'Difficoltà-3':
+            griglia(49);
+                break;
+        default:
+                break;
+        }
+        
         for (let i = 0; i < displayShow.length; i++) {
             displayShow[i].classList.toggle('d-flex');
             displayShow[i].classList.remove('back-ground');
