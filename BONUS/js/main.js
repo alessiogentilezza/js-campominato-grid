51b255d56e0b6a0742ec2e4da3d2701c73af377c
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-04 16:01:11 +0100

    create 2 funzioni, una per la creazione della griglia, e l'altra per generala a seconda della difficoltà scelta dall'utente

--- BONUS/js/main.js
+++ BONUS/js/main.js
@@ -2,22 +2,28 @@ const gridDom = document.getElementById('grid');
 const playGrid = document.getElementById('button');
 const casellaSelezione = document.getElementById('casellaSelezione');
 
-function griglia(numbreSquare){
+// funzione per creazione griglia e colore al click con variabile per le quantità
+
+function griglia(numbreSquare) {
 
     gridDom.innerHTML = '';
 
-    for (let i = 1; i <= numbreSquare; i++) {        
+    for (let i = 1; i <= numbreSquare; i++) {
         let elementoGriglia = document.createElement('div');
-        elementoGriglia.classList.add('square');
+        elementoGriglia.classList.add("square");
         elementoGriglia.append(i);
         gridDom.append(elementoGriglia);
-        
+
         elementoGriglia.addEventListener('click',
-        function () {
-            this.classList.toggle('back-ground');
-            console.log(this.innerHTML);
-        });}
+            function () {
+                this.classList.toggle('bg-blue');
+                console.log(this.innerHTML);
+            });
+    }
 }
+
+// al click genera la griglia a seconda della difficoltà e nel valore inserito nella posizione precedente
+// poi assegna la classe 10, 9, 7 a seconda di quanti elementi voglio in linea
 
 playGrid.addEventListener('click',
 
@@ -26,23 +32,32 @@ playGrid.addEventListener('click',
         let casellaSelezioneScelta = casellaSelezione.value;
 
         switch (casellaSelezioneScelta) {
-        
-        case 'Difficoltà-1':
-            griglia(100);
+
+            case 'Difficoltà-1':
+                griglia(100);
+                for (let i = 0; i < displayShow.length; i++) {
+                    displayShow[i].classList.add("linea10");
+
+                }
                 break;
-        case 'Difficoltà-2':
-            griglia(81);
+            case 'Difficoltà-2':
+                griglia(81);
+                for (let i = 0; i < displayShow.length; i++) {
+                    displayShow[i].classList.add("linea9");
+                }
                 break;
-        case 'Difficoltà-3':
-            griglia(49);
+            case 'Difficoltà-3':
+                griglia(49);
+                for (let i = 0; i < displayShow.length; i++) {
+                    displayShow[i].classList.add("linea7");
+                }
                 break;
-        default:
+            default:
                 break;
         }
-        
+
         for (let i = 0; i < displayShow.length; i++) {
             displayShow[i].classList.toggle('d-flex');
-            displayShow[i].classList.remove('back-ground');
         }
 
     });
